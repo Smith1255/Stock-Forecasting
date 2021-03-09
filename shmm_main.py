f9bd1a0a1d412e15bd514dddc4bccd41b4fe454f
Andrew Smith <andy-smith@comcast.net>
2021-03-08 21:36:16 -0500

    Clean up

--- shmm_main.py
+++ shmm_main.py
@@ -10,35 +10,33 @@ import argparse
 
 import stock_info
 
-
-likelihood_vect = np.empty([0,1])
-aic_vect = np.empty([0,1])
-bic_vect = np.empty([0,1])
-
-# Possible number of states in Markov Model
-STATE_SPACE = range(2,15)
-
 # Calculating Mean Absolute Percentage Error of predictions
 def calc_mape(predicted_data, true_data):
     return np.divide(np.sum(np.divide(np.absolute(predicted_data - true_data), true_data), 0), true_data.shape[0])
 
 
 def predict_prices(dataset, num_test=100, K = 50, num_iters=10000):
+    print("Defining the model...")
     model, opt_states = define_model(dataset=dataset, num_test=num_test, num_iters=num_iters)
     print("Finished defining the model")
+    print("Training the model...")
     predicted_stock_data = train_model(dataset=dataset, model=model, opt_states=opt_states, num_test=num_test, num_iters=num_iters, K=K)
     print("Finished training the model")
     return predicted_stock_data
 
 def define_model(dataset, num_test, num_iters):
+    # Possible number of states in Markov Model
+    STATE_SPACE = range(2,15)
     likelihood_vect = np.empty([0,1])
     aic_vect = np.empty([0,1])
     bic_vect = np.empty([0,1])
+
     for states in STATE_SPACE:
         num_params = states**2 + states
         dirichlet_params_states = np.random.randint(1,50,states)
         model = hmm.GaussianHMM(n_components=states, covariance_type='full', tol=0.0001, n_iter=num_iters)
-        model.fit(dataset[num_test:,:])
+        # model.fit(dataset[num_test:,:])
+        model.fit(dataset)
         if model.monitor_.iter == num_iters:
             print('Increase number of iterations')
             sys.exit(1)
@@ -51,7 +49,6 @@ def define_model(dataset, num_test, num_iters):
 
 def train_model(dataset, model, opt_states, num_test, K, num_iters):
     predicted_stock_data = np.empty([0,dataset.shape[1]])
-
     for idx in reversed(range(num_test)):
             train_dataset = dataset[idx + 1:,:]
             test_data = dataset[idx,:]; 
@@ -76,7 +73,7 @@ def train_model(dataset, model, opt_states, num_test, K, num_iters):
 
             if model.monitor_.iter == num_iters:
                 print('Increase number of iterations')
-                sys.exit(1)
+                exit(1)
 
             iters = 1;
             past_likelihood = []
@@ -90,7 +87,7 @@ def train_model(dataset, model, opt_states, num_test, K, num_iters):
 
     return predicted_stock_data
 
-def plot_data(predicted_stock_data, dataset, symbol, plot_all_features=False):
+def plot_data(predicted_stock_data, dataset, ticker, plot_all_features=False):
     labels = ['Close','Open','High','Low']
     if plot_all_features:
         for i in range(4):
@@ -99,7 +96,7 @@ def plot_data(predicted_stock_data, dataset, symbol, plot_all_features=False):
             plt.plot(range(100),np.flipud(dataset[range(100),i]),'r--', label = 'Actual '+labels[i]+' price')
             plt.xlabel('Time steps')
             plt.ylabel('Price')
-            plt.title(labels[i]+' price'+ ' for '+symbol[:-4])
+            plt.title(labels[i]+' price'+ ' for '+ticker[:-4])
             plt.grid(True)
             plt.legend(loc = 'upper left')
     else:
@@ -121,14 +118,10 @@ def plot_data(predicted_stock_data, dataset, symbol, plot_all_features=False):
 def get_arg_parser():
     args = argparse.ArgumentParser(description='Analyzes a given stock and its price history to generate predictions for future performance')
 
-    args.add_argument('Symbol',
-                           metavar='symbol',
-                           type=str,
-                           help='the stock symbol to analyze')
-    # args.add_argument('Symbol',
-    #                        metavar='symbol',
-    #                        type=str,
-    #                        help='the stock symbol to analyze')
+    args.add_argument('-t', '--ticker', type=str, help="the stock ticker to analyze")
+    args.add_argument('-i', '--sample_internal', nargs='?', const=1440, type=int, help="the sample interval of market data, in minutes. Default is a day")
+    args.add_argument('-ni', '--num_input', nargs='?', const=None, type=int, help="the number of data points to be used in the model. Must be >200.")
+    args.add_argument('-nr', '--num_results', nargs='?', const=100, type=int, help="the number of data points in the prediction data")
     args.add_argument('-s',
                        '--save',
                        action='store_true',
@@ -145,53 +138,41 @@ def get_arg_parser():
 
 def main():
     args = get_arg_parser().parse_args()
-    symbol = args.Symbol
-    print("Retrieving data for", symbol)
+    ticker = args.ticker
+    print("Retrieving data for", ticker)
     dataset = np.genfromtxt('apple.csv', delimiter=',')
     # try:
-    #     dataset = stock_info.get_stock_info(symbol=symbol).values
+    #     dataset = stock_info.get_stock_info(ticker=ticker).values
     # except ValueError as e:
     #     print ("Something went wrong while retrieving the price data.")
     #     print ("Error was:", str(e))
     #     print("Exiting.")
     #     exit()
+    print("Successfully retrieved data for", ticker)
 
-    print("Successfully retrieved data for", symbol)
-    predicted_data = predict_prices(dataset)
+    subset_size = args.num_input
+    if (not subset_size):
+        subset_size = len(dataset) - 1
+    elif (args.num_input) < 201:
+        print ("To sucessfully train the model, there need to be more than 200 data points.\nExiting")
+        exit()
+    elif (args.num_input > len(dataset)):
+        print (f'The number of input: {args.num_input} is greater than the data set size: {len(dataset)}.\nExiting')
+        exit()
+
+    data_subset = dataset[:subset_size]
+    predicted_data = predict_prices(data_subset)
 
     if (args.save):
-        filename = '{}_forecast.csv'.format(symbol)
+        filename = '{}_forecast.csv'.format(ticker)
         print("Saving to", filename)
         np.savetxt(filename, predicted_data, delimiter=',', fmt='%.2f')
 
     mape = calc_mape(predicted_data, np.flipud(dataset[range(100),:]))
-    print('\nMAPE for the stock {} is '.format(symbol), mape)
+    print('\nMAPE for the stock {} is '.format(ticker), mape)
         
     if (not args.no_plot):
-        plot_data(predicted_stock_data=predicted_data, dataset=dataset, symbol=symbol, plot_all_features=args.plot_all)
-
-    # result_queue = multiprocessing.Queue()
-    # p1 = multiprocessing.Process(target=predict_prices, args=(dataset, result_queue)) 
-    # p1.start()
-    # while (p1.is_alive()):
-    #     animated_loading()
-    # p1.join()
-    # with concurrent.futures.ThreadPoolExecutor() as executor:
-    #     future = executor.submit(predict_prices, dataset)
-
-    #     while(future.running()):
-    #         animated_loading()
-
-    #     if (args.save and future.done()):
-    #         filename = '{}_forecast.csv'.format(symbol)
-    #         print("\nSaving to", filename)
-    #         np.savetxt(filename, future.result(), delimiter=',', fmt='%.2f')
-
-    #     mape = calc_mape(future.result(), np.flipud(dataset[range(100),:]))
-    #     print('\nMAPE for the stock {} is '.format(symbol), mape)
-            
-    #     if (not args.no_plot):
-    #         plot_data(predicted_stock_data=future.result(), dataset=dataset, symbol=symbol, plot_all_features=args.plot_all)
+        plot_data(predicted_stock_data=predicted_data, dataset=dataset, ticker=ticker, plot_all_features=args.plot_all)
 
 if __name__ == "__main__":
     main()
